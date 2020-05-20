99d2dcff3c4504d2b14b6e25ab0009e035d6fb88
messi17 <31600117+messi17@users.noreply.github.com>
2020-05-20 15:55:17 +0300

    Update function.js

--- function.js
+++ function.js
@@ -9,7 +9,7 @@ let opacity = 100;
 let sepia = 0;
 let dropshadow = 0;
  
-const video = document.getElementById("video");
+const imgture = document.getElementById("video");
 const resetAll = document.getElementById("resetAll");
  
 const slider1 = document.getElementById("slider1");
