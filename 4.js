9af4d8895767c5b9fe6e50e7f40aeabdcf83556d
messi17 <31600117+messi17@users.noreply.github.com>
2020-09-22 20:52:46 +0300

    Update and rename 3.js to 4.js

--- 4.js
+++ 4.js
@@ -9,7 +9,7 @@ let opacity = 100;
 let sepia = 0;
 let dropshadow = 0;
  
-const imgture = document.getElementById("video");
+const imgture = document.getElementById("bitmovinplayer-video-player");
 const resetAll = document.getElementById("resetAll");
  
 const slider1 = document.getElementById("slider1");
