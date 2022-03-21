a2a84735b1054871dbdf79f7b06c3bb1e3f5e115
messi17 <31600117+messi17@users.noreply.github.com>
2022-03-21 18:51:47 +0300

    Add files via upload

---
+++ yeni/settings_platform.js
@@ -0,0 +1,53 @@
+var externalSourceMatrice = [
+    {
+      priority: 3,
+      platform: "windows",
+      browser: "chrome",
+      sourcePriority: {
+        DASH: 1,
+        HLS: 1,
+        VHLS: 0
+      }
+    },
+    {
+      priority: 1,
+      platform: "ios",
+      browser: "safari",
+      sourcePriority: {
+        DASH: 0,
+        HLS: 1,
+        VHLS: 1
+      }
+    },
+    {
+      priority: 2,
+      platform: "any",
+      browser: "firefox",
+      sourcePriority: {
+        DASH: 1,
+        HLS: 0,
+        VHLS: 0
+      }
+    },
+    {
+      priority: 5,
+      platform: "any",
+      browser: "any",
+      sourcePriority: {
+        DASH: 1,
+        HLS: 1,
+        VHLS: 0
+      }
+    },
+    {
+      priority: 4,
+      platform: "any",
+      browser: "opera",
+      sourcePriority: {
+        DASH: 0,
+        HLS: 1,
+        VHLS: 0
+      }
+    }
+  ];
+
