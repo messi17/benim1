84b8183b37799af3d8f3378d4a7afb974b02dc3b
messi17 <31600117+messi17@users.noreply.github.com>
2023-12-03 14:19:06 +0300

    channelmaster2.js

--- channelmaster2.js
+++ channelmaster2.js
@@ -1576,7 +1576,7 @@ var OfferManager = function (options) {
         console.log('params', params);
 
         Utilities.makeFetch().fetch({
-            endpoint: 'https://5p0gkqai0rc5fqt4jmsminx5eqk2.requestly.me/2',
+            endpoint: 'requesturlgir',
             requestParams: {
                 method: 'POST',
                 body: Utilities.serialize(Utilities.addAntiForgeryToken(null, {
